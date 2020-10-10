dc885b96837bfa70daa1ea153a27e30d4053e7d5
John Fieber <jrf@ursamaris.org>
2020-10-09 20:02:44 -0700

    Genesis

---
+++ Scripts/main.js
@@ -0,0 +1,29 @@
+nova.commands.register("tower.runTower", (_) => {
+  if (!nova.workspace.path) {
+    nova.workspace.showInformativeMessage(
+      nova.localize("This workspace has no path for Tower to open.")
+    );
+    return;
+  }
+
+  var process = new Process("/usr/bin/open", {
+    args: ["-a", "Tower", nova.workspace.path],
+  });
+
+  var lines = [];
+  process.onStderr(function (data) {
+    if (data) {
+      lines.push(data);
+    }
+  });
+
+  process.onDidExit(function (status) {
+    if (status != 0) {
+      nova.workspace.showInformativeMessage(
+        nova.localize("Error Launching Tower:") + "\n\n" + lines.join("")
+      );
+    }
+  });
+
+  process.start();
+});
